5009f2b74ced7e858b6088f799b3484eace51ac4
Lehrer, Brian <661570+blehrer@users.noreply.github.com>
2025-01-21 18:47:10 -0800

    restructure w/ .chezmoiroot

--- setup/apt.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo "This will install chezmoi and github.com/blehrer/dotfiles"
-cd ~/.local
-echo "... Installing ~/.local/bin/chezmoi"
-sh -c "$(curl -fsLS get.chezmoi.io)"
-echo "... Cloning dotfiles to ~/.local/share/chezmoi"
-./bin/chezmoi init --apply blehrer
