e180e35c0356f98e590b69b6c58a7b7d994e1979
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-06-19 20:57:41 -0700

    Update home/.config/kitty/kittens/cz_edit_zshrc.py

--- home/private_dot_config/kitty/kittens/cz_edit_zshrc.py
+++ home/private_dot_config/kitty/kittens/cz_edit_zshrc.py
@@ -7,7 +7,7 @@ def main(args):
     home = os.getenv('HOME')
     cz_lx1 = os.path.join(home, '.local', 'bin', 'chezmoi')
     cz_lx2 = os.path.join(home, '.bin', 'chezmoi')
-    cz_brew = Path.absolute('/opt/homebrew/bin/chezmoi')
+    cz_brew = os.path.join('opt', 'homebrew', 'bin', 'chezmoi')
     for path in [cz_brew, cz_lx1, cz_lx2]:
         if os.path.exists(path):
             os.system([path, 'edit', os.path.join(home, '.zshrc')])
