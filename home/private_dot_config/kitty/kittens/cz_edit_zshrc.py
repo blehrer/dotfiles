d8c30d5b9cab4237b6a2ae8808c55163a292df1e
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-06-19 21:27:45 -0700

    Update home/.config/kitty/kittens/cz_edit_zshrc.py

--- home/private_dot_config/kitty/kittens/cz_edit_zshrc.py
+++ home/private_dot_config/kitty/kittens/cz_edit_zshrc.py
@@ -7,8 +7,8 @@ def main(args):
     cz_lx1 = os.path.join(home, '.local', 'bin', 'chezmoi')
     cz_lx2 = os.path.join(home, '.bin', 'chezmoi')
     cz_brew = '/opt/homebrew/bin/chezmoi'
-    paths = itertools.dropwhile(lambda p: not os.path.exists(p), [
-                                cz_brew, cz_lx1, cz_lx2])
+    paths = list(itertools.dropwhile(lambda p: not os.path.exists(p), [
+        cz_brew, cz_lx1, cz_lx2]))
     if len(paths) > 0:
         os.system(f'{paths[0]} edit {os.path.join(home, ".zshrc")}')
 
