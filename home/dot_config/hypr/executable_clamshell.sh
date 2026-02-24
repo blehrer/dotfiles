bafc015da690f88c5290d28eff15e5d6ee5ddaf5
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2026-02-23 18:56:41 -0800

    Update .config/hypr/bindings.conf Update .config/hypr/clamshell.sh Update .config/hypr/monitors.conf

--- home/dot_config/hypr/executable_clamshell.sh
+++ home/dot_config/hypr/executable_clamshell.sh
@@ -4,12 +4,16 @@ LAPTOP_DISPLAY="eDP-1"
 HDMI_DISPLAY="HDMI-A-1"
 
 # notify-send "clamshell.sh: init (1: $1)"
-
-if [ "$1" = "open" ]; then
-    hyprctl keyword monitor "${LAPTOP_DISPLAY},preferred,auto-left,1"
-    notify-send "clamshell.sh: open"
-elif [ "$1" = "close" ]; then
-    hyprctl keyword monitor "${LAPTOP_DISPLAY},disable"
-    notify-send "clamshell.sh: closed"
+monitorCount=$(hyprctl monitors -j | jq 'length')
+if [[ $monitorCount != "1" ]]; then
+    if [ "$1" = "open" ]; then
+        hyprctl keyword monitor "${LAPTOP_DISPLAY},preferred,auto-left,1.6"
+        notify-send "clamshell.sh: open"
+    elif [ "$1" = "close" ]; then
+        hyprctl keyword monitor "${LAPTOP_DISPLAY},disable"
+        notify-send "clamshell.sh: closed"
+    fi
+elif 
+    hyprctl keyword monitor "${HDMI_DISPLAY},disable"
+    hyprctl keyword monitor "${LAPTOP_DISPLAY},preferred,auto,1.6"
 fi
-
